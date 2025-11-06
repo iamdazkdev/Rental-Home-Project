3321b24b6d2350e0415d260fe992402a5af5f3a4
Hà Lưu Văn Ánh <anhaaa2305@gmail.com>
2025-11-06 21:55:45 +0700

    Add centralized error codes and response helpers for consistent API error handling

---
+++ server/constants/index.js
@@ -0,0 +1,16 @@
+/**
+ * Constants Index File
+ * Central export point for all application constants
+ */
+
+const errorCodes = require('./errorCodes');
+
+module.exports = {
+  ...errorCodes,
+  
+  // You can add more constant files here as your project grows
+  // For example:
+  // ...statusCodes,
+  // ...apiRoutes,
+  // ...configValues,
+};
