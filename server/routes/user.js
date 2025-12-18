3d6944424e631f4f5d069ae3db950a32725bd8d6
Hà Lưu Văn Ánh <anhaaa2305@gmail.com>
2025-12-18 14:19:49 +0700

    feat: implement wishlist functionality for users to add or remove listings

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
 const Booking = require("../models/Booking");
+const User = require("../models/User");
+const Listing = require("../models/Listing");
 const { HTTP_STATUS } = require("../constants");
 
 // GET TRIP LIST
@@ -19,4 +21,36 @@ router.get("/:userId/trips", async (req, res) => {
   }
 });
 
+// ADD LISTING TO WISHLIST
+router.patch("/:userId/:listingId", async (req, res) => {
+  try {
+    const { userId, listingId } = req.params;
+    const user = await User.findById(userId);
+    const listing = await Listing.findById(listingId);
+
+    const favoriteListing = user.wishList.find(
+      (item) => item.id.toString() || item._id.toString() === listingId
+    );
+    if (favoriteListing) {
+      user.wishList = user.wishList.filter(
+        (item) => iitem.id.toString() || item._id.toString() !== listingId
+      );
+      await user.save();
+      res
+        .status(HTTP_STATUS.OK)
+        .json({ message: "Removed from wishlist", wishList: user.wishList });
+    } else {
+      user.wishList.push(listing);
+      await user.save();
+      res
+        .status(HTTP_STATUS.OK)
+        .json({ message: "Added to wishlist", wishList: user.wishList });
+    }
+  } catch (err) {
+    console.log("ERROR: Fail to add listing to wishlist", err);
+    res
+      .status(HTTP_STATUS.NOT_FOUND)
+      .json({ message: "Trips not found", error: err.message });
+  }
+});
 module.exports = router;
