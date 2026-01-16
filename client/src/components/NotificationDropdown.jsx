9a8968ee8a971436cc1af9661d3764b9663797e7
Hà Lưu Văn Ánh <anhaaa2305@gmail.com>
2026-01-16 15:46:48 +0700

    feat: update import paths in NotificationDropdown component

--- client/src/components/NotificationDropdown.jsx
+++ client/src/components/NotificationDropdown.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-import {useSocket} from '../../context/SocketContext';
-import {CONFIG, HTTP_METHODS} from '../../constants/api';
+import {useSocket} from '../context/SocketContext';
+import {CONFIG, HTTP_METHODS} from '../constants/api';
 import './NotificationDropdown.scss';
 
 const NotificationDropdown = ({user}) => {
